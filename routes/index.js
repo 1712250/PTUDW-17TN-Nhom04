f0b8372f12812cd3d27f09049ecd6ff63cdd0104
lhvubtqn <lhvubtqn@gmail.com>
2020-06-26 01:55:33 +0700

    separate browse rounter from index router

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,13 @@
 const router = require("express").Router();
 const checkout = require("./checkout");
 const account = require("./account");
+const browse = require("./browse");
 
 router.get("/", (req, res, next) => {
 	res.render("homepage", { title: "Obooks" });
 });
 
-router.get("/browse", (req, res, next) => {
-	res.render("browse_books", { title: "Browse" });
-});
-
-router.get("/browse/book", (req, res, next) => {
-	res.render("book_detail", { title: "Book Detail" });
-});
-
+router.use("/browse", browse);
 router.use("/checkout", checkout);
 router.use("/account", account);
 
