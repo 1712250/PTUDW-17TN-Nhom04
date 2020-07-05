d2b6b83b158fe5270aedee4b281e41302194663a
lhvubtqn <lhvubtqn@gmail.com>
2020-07-05 21:22:52 +0700

    auto change baseUrl on production

--- utils/MailController.js
+++ utils/MailController.js
@@ -1,5 +1,8 @@
 const transporter = require("../config/mail");
 
+const baseUrl = process.env.DEV
+	? "http://localhost:5000"
+	: "https://obooks.herokuapp.com";
 module.exports.sendActivation = function (tempUser) {
 	transporter.sendMail({
 		from: "ptudw.obooks@gmail.com",
@@ -9,17 +12,8 @@ module.exports.sendActivation = function (tempUser) {
 			"Hi there\n\n" +
 			"We are happy that you choose to use Obooks.\n\n" +
 			"You can activate your account here:\n" +
-			`http://localhost:5000/account/activation/${tempUser._id}\n\n` +
+			`${baseUrl}/account/activation/${tempUser._id}\n\n` +
 			"Sincerely,\n" +
 			"Obooks",
-	});
-};
-
-module.exports.sendMail = function (recipient, title, plaintext) {
-	transporter.sendMail({
-		from: "ptudw.obooks@gmail.com",
-		to: recipient, //"recipient@example.com",
-		subject: title,
-		text: "http://localhost:5000/api/auth/forgetpassword",
 	});
 };
