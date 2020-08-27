037a3216c870f6ecbb2d8a6115a760545bf11159
vinh <vinhvipro003@gmail.com>
2020-08-27 20:48:32 +0700

    test

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module admin
 
 go 1.14
+
+require (
+	github.com/gin-gonic/gin v1.6.3
+	github.com/heroku/x v0.0.25
+)
--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"log"
-	"net/http"
 	"os"
 
 	"github.com/gin-gonic/gin"
@@ -18,11 +17,8 @@ func main() {
 
 	router := gin.New()
 	router.Use(gin.Logger())
-	router.LoadHTMLGlob("templates/*.tmpl.html")
-	router.Static("/static", "static")
-
-	router.GET("/", func(c *gin.Context) {
-		c.HTML(http.StatusOK, "index.tmpl.html", nil)
+	router.GET("/", func(r *gin.Context) {
+		r.String(200, "ok")
 	})
 
 	router.Run(":" + port)
