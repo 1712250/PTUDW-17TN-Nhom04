7c581f4fe101a3c1511ae28a86c5b217b7e1c744
1712250 <lhvubtqn@gmail.com>
2020-08-29 15:41:45 +0700

    .

--- app.js
+++ app.js
@@ -25,9 +25,9 @@ require("./config/database");
 require("./config/mail");
 
 // redirect when use http
-app.use("**", (req, res, next) => {
-  if (!req.secure)
-    res.redirect('https://' + req.headers.host + req.url);
+app.use("*", (req, res, next) => {
+  if (req.protocol === "https")
+    res.redirect(301, 'https://' + req.headers.host + req.url);
   else 
     next();
 });
